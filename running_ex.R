10a39f23f6589f80966f694be24465d8ad439372
pdwaggoner <philip.waggoner@gmail.com>
2024-11-04 14:54:58 -0700

    updating and adding new sim scenario for testing/chks

--- running_ex.R
+++ running_ex.R
@@ -1,6 +1,9 @@
 # load current pkg version
 devtools::load_all()
 
+
+##
+## SCENARIO 1
 # define some simple/toy vals and data for quick testing
 puff_dt <- 60  # puff creation interval in seconds
 sim_dt <- 60  # sim time step in seconds (e.g., 60 = 1-minute intervals)
@@ -38,7 +41,7 @@ sensor_coords <- matrix(c(200, 200, 0,
                         ncol = 3, byrow = TRUE)
 
 # try it out
-sensor_simulation <- simulate_sensor_mode(
+sensor_simulation1 <- simulate_sensor_mode(
   time_stamps_sim = time_stamps_sim,
   sensor_coords = sensor_coords,
   n_sim = n_sim,
@@ -48,4 +51,77 @@ sensor_simulation <- simulate_sensor_mode(
   wind_data = wind_data,
   puff_duration = 1200)
 
-print(sensor_simulation)
+
+##
+## SCENARIO 2
+puff_dt <- 60
+sim_dt <- 60
+sim_start <- "2024-02-01 00:00:00"
+sim_end <- "2024-02-01 01:00:00"
+
+# create time stamps
+time_stamps_sim <- seq(as.POSIXct(sim_start), as.POSIXct(sim_end), by = sim_dt)
+n_sim <- length(time_stamps_sim)
+
+# toy wind data
+wind_speeds <- c(2.5, 3.5, 4.5, 3.5, 2.5)  # speed in m/s, with more variation
+wind_directions <- c(80, 95, 120, 95, 80)  # directions in degrees, more spread out
+
+wind_data <- interpolate_wind_data(wind_speeds, wind_directions,
+                                   sim_start, sim_end,
+                                   puff_dt)
+
+# source coordinates and emission properties
+source_coords <- matrix(c(
+  20, 30, 1.5,
+  70, 20, 2.5,
+  45, 80, 4
+), ncol = 3, byrow = TRUE)
+
+# emission frequency for the sources (more variable)
+emission_freq <- c(0.2, 0.4, 0.6)
+
+# new sensor coordinates, spread out further and adjusted for coverage
+sensor_coords <- matrix(c(
+  150, 250, 0,
+  350, 350, 0,
+  450, 450, 0,
+  500, 100, 0
+), ncol = 3, byrow = TRUE)
+
+# try it out
+sensor_simulation2 <- simulate_sensor_mode(
+  time_stamps_sim = time_stamps_sim,
+  sensor_coords = sensor_coords,
+  n_sim = n_sim,
+  num_sources = nrow(source_coords),
+  source_coords = source_coords,
+  emission_rates = emission_freq,
+  wind_data = wind_data,
+  puff_duration = 1200)
+
+
+##
+## viz idea
+library(ggplot2)
+
+time_steps <- nrow(sensor_simulation2)
+sensor_ids <- c("Sensor 1", "Sensor 2", "Sensor 3", "Sensor 4")
+
+sensor_data <- data.frame(
+  time = rep(1:time_steps, each = 4),
+  sensor_id = rep(sensor_ids, time_steps),
+  concentration = as.vector(sensor_simulation2)
+)
+
+sensor_data |>
+  dplyr::filter(time < 20) |>
+  ggplot(aes(x = time, y = concentration,
+             color = sensor_id)) +
+  geom_line(aes(group = sensor_id), linewidth = 1) +
+  facet_wrap(~sensor_id) +
+  geom_point(aes(shape = sensor_id), size = 2) +
+  labs(title = "Methane Emission Dispersion Across Sensors Over 20 Seconds",
+       x = "Time (s)",
+       y = "Methane Concentration") +
+  theme_minimal()
