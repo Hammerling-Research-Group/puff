669a45f4fbf863caa072c743a2bbde4f15bfdba0
teaganward <teaganward@mines.edu>
2024-11-07 12:18:49 -0700

    Single Sensor Example

--- running_ex.R
+++ running_ex.R
@@ -2,6 +2,71 @@
 devtools::load_all()
 
 
+## SINGLE SENSOR IMPLEMENTATION
+# define some simple/toy vals and data for quick testing
+puff_dt <- 60  # puff creation interval in seconds
+sim_dt <- 60  # sim time step in seconds (e.g., 60 = 1-minute intervals)
+sim_start <- "2024-03-01 00:00:00"
+sim_end <- "2024-03-01 01:00:00"
+
+# create time stamps
+time_stamps_sim <- seq(as.POSIXct(sim_start), as.POSIXct(sim_end), by = sim_dt)
+n_sim <- length(time_stamps_sim)
+
+# toy wind data (*will update with real wind data once code is stable)
+wind_speeds <- c(1, 2, 4, 4, 2)  # speed in m/s
+wind_directions <- c(85, 100, 115, 100, 90)  # directions in degrees
+
+# Interpolate wind data to match the simulation resolution
+wind_data <- interpolate_wind_data(wind_speeds, wind_directions,
+                                   sim_start, sim_end,
+                                   puff_dt)
+
+## define emissions scenarios based on Meng's placement code (step 1)
+# source coordinates and emission properties
+source_coords <- matrix(c(
+  80, 50, 3  # source A at (80, 50, 3)
+), ncol = 3, byrow = TRUE)
+
+# emission frequency for the source (p1)
+emission_freq <- c(0.3)
+
+# def 1 sensor coordinate as a start
+sensor_coords <- matrix(c(150, 150, 0),
+                        ncol = 3, byrow = TRUE)
+
+# try it out
+simple_sensor_simulation <- simulate_sensor_mode(
+  time_stamps_sim = time_stamps_sim,
+  sensor_coords = sensor_coords,
+  n_sim = n_sim,
+  num_sources = nrow(source_coords),
+  source_coords = source_coords,
+  emission_rates = emission_freq,
+  wind_data = wind_data,
+  puff_duration = 1200)
+
+# Load necessary library for plotting
+library(ggplot2)
+
+# Prepare the data for plotting
+# Convert time stamps to a data frame along with simulated concentration
+sensor_data <- data.frame(
+  time = time_stamps_sim,
+  concentration = simple_sensor_simulation[, 1]  # Assuming one sensor for simplicity
+)
+
+# Plot the methane concentration at the sensor over time
+ggplot(sensor_data, aes(x = time, y = concentration)) +
+  geom_line(color = "blue") +
+  labs(
+    title = "Methane Emission Dispersion",
+    x = "Time (min)",
+    y = "Methane Concentration (ppm)"
+  ) +
+  theme_minimal()
+
+
 ##
 ## SCENARIO 1
 # define some simple/toy vals and data for quick testing
