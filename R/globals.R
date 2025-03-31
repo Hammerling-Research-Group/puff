08eba454d4ff52ee219f2b3bb7b498a198570487
pdwaggoner <philip.waggoner@gmail.com>
2025-03-31 14:06:46 -0600

    adj global vars

--- R/globals.R
+++ R/globals.R
@@ -2,4 +2,5 @@ utils::globalVariables(c("Concentration", "Group.1", "Sensor", "Sensor_1",
                          "concentration", "isomax", "isomin", "time2",
                          "timestamp", "type", "value", "variable", "wind_u",
                          "wind_v", "x", "y", "direction", "wind_speed",
-                         "mean_wind_speed", "direction_vec"))
+                         "mean_wind_speed", "direction_vec",
+                         "xend", "yend", "label", "count", "mean_speed"))
